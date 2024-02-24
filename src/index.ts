967d870ee6d41f7ad8c66a67a0d01c6054e6f5df
onze <slapexs@gmail.com>
2024-02-24 22:21:29 +0700

    IMP: Init credit service

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+const PORT = process.env.PORT!;
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+app.guard(
+	{
+		beforeHandle({ set, cookie: { accessToken } }) {
+			console.log(accessToken);
+			if (!accessToken.value) {
+				set.status = "Unauthorized";
+				return { status: "Unauthorized" };
+			}
+		},
+	},
+	(app) => {
+		return app.group("/api/v1/credit", (router) => {
+			return router.get("/", () => "Hello credit service").get("/asd", () => ({ msg: "asd" }));
+		});
+	}
 );
+
+app.listen(PORT);
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
+
